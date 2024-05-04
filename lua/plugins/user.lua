a3e1621c18a3af358d05a5e3ab8ff1e7f1277027
Vo Quang Chien <voquangchien.dev@proton.me>
2024-05-04 22:32:17 +0700

    feat(cmp): use `Tab` as confirm key

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -65,4 +65,21 @@ return {
     "tokyonight.nvim",
     enabled = false,
   },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      })
+    end,
+  },
 }
