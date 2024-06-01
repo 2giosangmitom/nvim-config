f29e5197e39f6937ffe6c86f9b7078e3f17e16b9
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-01 20:28:26 +0700

    update config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -101,6 +101,24 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, { "typstfmt", "golangci-lint" })
+      opts.ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = "󰄳 ",
+          package_uninstalled = " 󰚌",
+        },
+
+        keymaps = {
+          toggle_server_expand = "<CR>",
+          install_server = "i",
+          update_server = "u",
+          check_server_version = "c",
+          update_all_servers = "U",
+          check_outdated_servers = "C",
+          uninstall_server = "X",
+          cancel_installation = "<C-c>",
+        },
+      }
     end,
   },
 
