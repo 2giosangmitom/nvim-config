3131cf5ffc1a46eeb7740a6d25849cfc22637d06
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-12 08:33:38 +0700

    update

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,62 @@
 return {
   {
+    'Saecki/crates.nvim',
+    event = { 'BufRead Cargo.toml' },
+    opts = {
+      completion = {
+        cmp = { enabled = true },
+      },
+    },
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set('n', '<leader>ca', function()
+            vim.cmd.RustLsp('codeAction')
+          end, { desc = 'Code Action', buffer = bufnr })
+          vim.keymap.set('n', '<leader>dr', function()
+            vim.cmd.RustLsp('debuggables')
+          end, { desc = 'Rust Debuggables', buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = 'clippy',
+              extraArgs = { '--no-deps' },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ['async-trait'] = { 'async_trait' },
+                ['napi-derive'] = { 'napi' },
+                ['async-recursion'] = { 'async_recursion' },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
+    end,
+  },
+
+  {
     'ray-x/go.nvim',
     dependencies = {
       'ray-x/guihua.lua',
@@ -29,6 +86,21 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     dependencies = {
       'williamboman/mason.nvim',
+      {
+        'nvimdev/lspsaga.nvim',
+        opts = {
+          symbol_in_winbar = {
+            show_file = true,
+            folder_level = 0,
+          },
+          hover = {
+            open_cmd = '!xdg-open',
+          },
+          lightbulb = {
+            enable = false,
+          },
+        },
+      },
     },
     opts = function()
       local icons = require('config.icons')
@@ -58,6 +130,7 @@ return {
       }
     end,
     config = function(_, opts)
+      local utils = require('utils')
       -- Border
       vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = opts.ui.border })
       vim.lsp.handlers['textDocument/signatureHelp'] =
@@ -66,6 +139,27 @@ return {
 
       -- Diagnostics
       vim.diagnostic.config(opts.diagnostics)
+
+      -- Setup keymaps
+      utils.on_attach(function(_, bufnr)
+        local function map(key, cmd, desc)
+          vim.keymap.set('n', key, cmd, { desc = desc, buffer = bufnr })
+        end
+        map('K', '<cmd>Lspsaga hover_doc<cr>', 'Hover')
+        map('<leader>ci', '<cmd>Lspsaga incoming_calls<cr>', 'Incoming calls')
+        map('<leader>co', '<cmd>Lspsaga outgoing_calls<cr>', 'Outgoing calls')
+        map('<leader>ca', '<cmd>Lspsaga code_action<cr>', 'Code action')
+        map('<leader>cd', '<cmd>Lspsaga peek_definition<cr>', 'Peek definition')
+        map('gd', '<cmd>Lspsaga goto_definition<cr>', 'Goto Definition')
+        map('gr', '<cmd>Telescope lsp_references<cr>', 'References')
+        map(']d', '<cmd>Lspsaga diagnostic_jump_next<cr>', 'Next diagnostic')
+        map('[d', '<cmd>Lspsaga diagnostic_jump_prev<cr>', 'Previous diagnostic')
+        map('<leader>cr', '<cmd>Lspsaga rename<cr>', 'Rename')
+        map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
+        map('gI', function()
+          require('telescope.builtin').lsp_implementations({ reuse_win = true })
+        end, 'Goto Implementation')
+      end)
 
       -- Setup servers
       for _, server in ipairs(opts.servers) do
@@ -121,6 +215,7 @@ return {
           { name = 'snippets' },
           { name = 'nvim_lsp' },
           { name = 'path' },
+          { name = 'crates' },
         }, {
           { name = 'buffer' },
         }),
