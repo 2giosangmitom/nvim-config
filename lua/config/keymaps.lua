d6fd360ce574d0ee9665dc62d61d73dfd19094c3
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-12 15:12:28 +0700

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,19 @@
 local map = vim.keymap.set
 
-map('n', '<c-s>', '<cmd>w<cr>', { desc = 'Save file' })
+-- Key mappings
+map('n', '<C-s>', '<cmd>w<cr>', { desc = 'Save file' })
 map('n', '<leader>pl', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
-map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
-map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
-
--- Move to window using the <ctrl> hjkl keys
-map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
-map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
-map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
-map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
+-- Window navigation
+local window_nav_commands = {
+  ['h'] = '<C-w>h',
+  ['j'] = '<C-w>j',
+  ['k'] = '<C-w>k',
+  ['l'] = '<C-w>l',
+}
+for key, command in pairs(window_nav_commands) do
+  map('n', '<C-' .. key .. '>', command, { desc = 'Go to ' .. key:upper() .. ' Window', remap = true })
+end
 
 -- Resize window using <ctrl> arrow keys
 map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
@@ -19,14 +22,15 @@ map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Wi
 map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 
 -- Move Lines
-map('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
-map('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
-map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
-map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+local move_line_command = '<cmd>m .%s<cr>=='
+map('n', '<A-j>', move_line_command:format('+1'), { desc = 'Move Down' })
+map('n', '<A-k>', move_line_command:format('-2'), { desc = 'Move Up' })
+map('i', '<A-j>', '<esc>' .. move_line_command:format('+1') .. 'gi', { desc = 'Move Down' })
+map('i', '<A-k>', '<esc>' .. move_line_command:format('-2') .. 'gi', { desc = 'Move Up' })
 map('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
 map('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
 
--- buffers
+-- Buffers
 map('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
@@ -36,31 +40,31 @@ map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
 -- Clear search with <esc>
 map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
 
-map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
-map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
-map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
-map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
-map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
-map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+-- Search navigation
+local search_nav_command = "'Nn'[v:searchforward].'zv'"
+map({ 'n', 'x', 'o' }, 'n', search_nav_command, { expr = true, desc = 'Next Search Result' })
+map({ 'n', 'x', 'o' }, 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
 
+-- Other mappings
 map('v', '<', '<gv')
 map('v', '>', '>gv')
 map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
 map('n', '<leader>xl', '<cmd>lopen<cr>', { desc = 'Location List' })
 map('n', '<leader>xq', '<cmd>copen<cr>', { desc = 'Quickfix List' })
-
 map('n', '[q', vim.cmd.cprev, { desc = 'Previous Quickfix' })
 map('n', ']q', vim.cmd.cnext, { desc = 'Next Quickfix' })
 
+-- Window management
 map('n', '<leader>ww', '<C-W>p', { desc = 'Other Window', remap = true })
 map('n', '<leader>wd', '<C-W>c', { desc = 'Delete Window', remap = true })
 map('n', '<leader>w-', '<C-W>s', { desc = 'Split Window Below', remap = true })
 map('n', '<leader>w|', '<C-W>v', { desc = 'Split Window Right', remap = true })
 
--- tabs
-map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
+-- Tab navigation
+local tab_nav_command = '<cmd>tab%s<cr>'
+map('n', '<leader><tab>l', tab_nav_command:format('last'), { desc = 'Last Tab' })
 map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
-map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
+map('n', '<leader><tab>f', tab_nav_command:format('first'), { desc = 'First Tab' })
 map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New Tab' })
 map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
 map('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
