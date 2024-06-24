1e51032148415ff92dacdbb91bcbe4ab54919d05
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-24 10:23:09 +0700

    chore: update comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,18 @@
 local map = vim.keymap.set
 
--- better up/down
+-- Better up/down movement
 map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
 map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
 map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
 map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
 
--- Move to window using the <ctrl> hjkl keys
+-- Move to window using the <Ctrl> hjkl keys
 map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
 map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
 map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
 map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
 
--- Resize window using <ctrl> arrow keys
+-- Resize window using <Ctrl> arrow keys
 map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
 map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
 map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
@@ -26,58 +26,46 @@ map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
 map('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
 map('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
 
--- buffers
+-- Buffers
 map('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 map('n', '<leader>bd', '<cmd>bd<cr>', { desc = 'Delete Buffer' })
 
--- Clear search with <esc>
-map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
+-- Clear search with <Esc>
+map({ 'i', 'n' }, '<Esc>', '<cmd>noh<cr><Esc>', { desc = 'Escape and Clear hlsearch' })
 
 -- Clear search, diff update and redraw
--- taken from runtime/lua/_editor.lua
-map(
-  'n',
-  '<leader>ur',
-  '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>',
-  { desc = 'Redraw / Clear hlsearch / Diff Update' }
-)
+map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / Clear hlsearch / Diff Update' })
 
--- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
-map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
-map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
-map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
-map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
-map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+-- Next and Previous Search Result
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { desc = 'Next Search Result', expr = true })
+map('x', 'n', "'Nn'[v:searchforward]", { desc = 'Next Search Result', expr = true })
+map('o', 'n', "'Nn'[v:searchforward]", { desc = 'Next Search Result', expr = true })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { desc = 'Prev Search Result', expr = true })
+map('x', 'N', "'nN'[v:searchforward]", { desc = 'Prev Search Result', expr = true })
+map('o', 'N', "'nN'[v:searchforward]", { desc = 'Prev Search Result', expr = true })
 
 -- Add undo break-points
 map('i', ',', ',<c-g>u')
 map('i', '.', '.<c-g>u')
 map('i', ';', ';<c-g>u')
 
--- save file
-map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+-- Save file
+map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><Esc>', { desc = 'Save File' })
 
---keywordprg
-map('n', '<leader>K', '<cmd>norm! K<cr>', { desc = 'Keywordprg' })
-
--- better indenting
+-- Better indenting
 map('v', '<', '<gv')
 map('v', '>', '>gv')
 
--- commenting
-map('n', 'gco', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add Comment Below' })
-map('n', 'gcO', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add Comment Above' })
-
--- lazy
+-- Lazy
 map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
--- new file
+-- New file
 map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
 
+-- Location and Quickfix Lists
 map('n', '<leader>xl', '<cmd>lopen<cr>', { desc = 'Location List' })
 map('n', '<leader>xq', '<cmd>copen<cr>', { desc = 'Quickfix List' })
 
@@ -85,15 +73,15 @@ map('n', '[q', vim.cmd.cprev, { desc = 'Previous Quickfix' })
 map('n', ']q', vim.cmd.cnext, { desc = 'Next Quickfix' })
 
 -- Terminal Mappings
-map('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter Normal Mode' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Enter Normal Mode' })
 map('t', '<C-h>', '<cmd>wincmd h<cr>', { desc = 'Go to Left Window' })
 map('t', '<C-j>', '<cmd>wincmd j<cr>', { desc = 'Go to Lower Window' })
 map('t', '<C-k>', '<cmd>wincmd k<cr>', { desc = 'Go to Upper Window' })
 map('t', '<C-l>', '<cmd>wincmd l<cr>', { desc = 'Go to Right Window' })
 map('t', '<C-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
-map('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
+map('t', '<C-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
 
--- windows
+-- Window Management
 map('n', '<leader>ww', '<C-W>p', { desc = 'Other Window', remap = true })
 map('n', '<leader>wd', '<C-W>c', { desc = 'Delete Window', remap = true })
 map('n', '<leader>w-', '<C-W>s', { desc = 'Split Window Below', remap = true })
@@ -101,7 +89,7 @@ map('n', '<leader>w|', '<C-W>v', { desc = 'Split Window Right', remap = true })
 map('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
 map('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
 
--- tabs
+-- Tab Management
 map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
 map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
 map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
