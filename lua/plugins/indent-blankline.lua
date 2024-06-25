3825c03ddc35a4d9135e885d0256511a12116e22
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-25 08:48:47 +0700

    feat: add `indent-blankline.nvim`

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,25 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  event = "VeryLazy",
+  opts = {
+    indent = {
+      char = '│',
+      tab_char = '│',
+    },
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        'help',
+        'dashboard',
+        'neo-tree',
+        'Trouble',
+        'trouble',
+        'lazy',
+        'mason',
+        'notify',
+        'toggleterm',
+      },
+    },
+  },
+  main = 'ibl',
+}
