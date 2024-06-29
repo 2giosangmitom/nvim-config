8c1d84edfb8e94e41bd254e2ad410385b4301863
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-29 17:12:27 +0700

    feat(lua): ensure `stylua` is installed

--- lua/extra/lua.lua
+++ lua/extra/lua.lua
@@ -59,4 +59,11 @@ return {
     'hrsh7th/nvim-cmp',
     opts = function(_, opts) table.insert(opts.sources, { name = 'lazydev', group_index = 0 }) end,
   },
+
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = { 'stylua' },
+    },
+  },
 }
