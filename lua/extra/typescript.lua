16087332f9d132043d7aaa49b4d524155629d736
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-30 15:17:47 +0700

    add typescript and vue support

--- lua/extra/typescript.lua
+++ lua/extra/typescript.lua
@@ -1,3 +1,47 @@
 return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = { 'javascript', 'typescript', 'tsx', 'jsdoc' },
+    },
+  },
 
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        vtsls = {},
+        eslint = {},
+      },
+    },
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = { ensure_installed = { 'prettierd' } },
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+      local langs = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }
+      for _, ft in ipairs(langs) do
+        opts.formatters_by_ft[ft] = { 'prettierd' }
+      end
+    end,
+  },
+
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    opts = {
+      ensure_installed = { 'js' },
+    },
+  },
+
+  {
+    'vuki656/package-info.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+    event = 'BufRead package.json',
+  },
 }
