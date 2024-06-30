cb81b60a9da4a076460078292361d5b7e0c99ec3
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-30 21:44:39 +0700

    add nix support

--- lua/extra/nix.lua
+++ lua/extra/nix.lua
@@ -1,1 +1,21 @@
-return {}
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        nil_ls = {
+          mason = false,
+        },
+      },
+    },
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        nix = { 'alejandra' },
+      },
+    },
+  },
+}
