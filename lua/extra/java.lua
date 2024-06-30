906336b9fbdc1f097b4d8de8a36965835eb218fd
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-30 21:44:44 +0700

    add java support

--- lua/extra/java.lua
+++ lua/extra/java.lua
@@ -1,1 +1,43 @@
-return {}
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = { 'java' },
+    },
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      registries = {
+        'github:nvim-java/mason-registry',
+        'github:mason-org/mason-registry',
+      },
+    },
+  },
+
+  {
+    'nvim-java/nvim-java',
+    config = false,
+    ft = { 'java' },
+    dependencies = {
+      {
+        'neovim/nvim-lspconfig',
+        opts = {
+          servers = {
+            jdtls = {},
+          },
+          setup = {
+            jdtls = function()
+              require('java').setup({
+                jdk = {
+                  auto_install = false,
+                },
+              })
+            end,
+          },
+        },
+      },
+    },
+  },
+}
