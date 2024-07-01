62c0f4e01ee26d3d64ba0e76f337c227cf8d53f4
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-01 14:10:32 +0700

    feat: update nightfall custom highlight

--- lua/plugins/nightfall.lua
+++ lua/plugins/nightfall.lua
@@ -22,10 +22,22 @@ return {
           DapStoppedLine = { default = true, link = 'Visual' },
         }
       end,
+      maron = function(colors)
+        return {
+          St_pos_sep = { fg = colors.green, bg = colors.deep_navy },
+          St_pos_icon = { fg = colors.black, bg = colors.green },
+          St_pos_text = { fg = colors.green, bg = colors.deep_navy },
+          St_cwd_icon = { fg = colors.black, bg = colors.light_cyan },
+          St_cwd_text = { fg = colors.light_cyan, bg = colors.deep_navy },
+          St_cwd_sep = { fg = colors.light_cyan, bg = colors.statusline_bg },
+          St_lspSv = { fg = colors.peach, bg = colors.statusline_bg },
+          DapStoppedLine = { default = true, link = 'Visual' },
+        }
+      end,
     },
   },
   config = function(_, opts)
     require('nightfall').setup(opts)
-    vim.cmd('colorscheme nightfall')
+    vim.cmd('colorscheme maron')
   end,
 }
