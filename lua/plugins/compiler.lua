92fa2913d2bc3d96bc4204cae792f3d91127339c
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-18 15:05:59 +0700

    update keymap

--- lua/plugins/compiler.lua
+++ lua/plugins/compiler.lua
@@ -4,7 +4,7 @@ return {
   dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
   keys = {
     { "<leader>cb", "<cmd>CompilerOpen<cr>", desc = "Open compiler" },
-    { "<leader>uc", "<cmd>CompilerToggleResults<cr>", desc = "Toggle compiler result" },
+    { "<leader>uB", "<cmd>CompilerToggleResults<cr>", desc = "Toggle compiler result" },
   },
   opts = {},
 }
